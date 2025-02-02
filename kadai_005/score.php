e2dc1698e35426c7e1224642990126f51706fa02
aphex450 <y-yokoo@aphex-group.com>
2025-02-02 18:18:07 +0900

    課題005アップロード

---
+++ kadai_005/score.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="discription" content="PHP課題005です">
+  <title>PHP課題005</title>
+</head>
+<body>
+
+<P>
+<?php
+
+  $score_1 = 80;
+  $score_2 = 60;
+  $score_3 = 55;
+  $score_4 = 40;
+  $score_5 = 100;
+  $score_6 = 25;
+  $score_7 = 80;
+  $score_8 = 95;
+  $score_9 = 30;
+  $score_10 = 60;
+
+  $score = ($score_1+$score_2+$score_3+$score_4+$score_5+$score_6+$score_7+$score_8+$score_9+$score_10);
+
+  echo $score/10;
+
+  ?>
+
+
+</P>
+  
+</body>
+</html>
