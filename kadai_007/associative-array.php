f62e7ed7e841114a7c86e4b3775086c2d21c958e
aphex450 <y-yokoo@aphex-group.com>
2025-02-07 01:26:49 +0900

    kadai_007アップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="discription" content="width=PHPの課題007です">
+  <title>PHP課題007</title>
+</head>
+<body>
+  
+<?php
+
+$product = ['name' => 'onion','price' => 200 ,'weight' => 160];
+
+print_r($product);
+
+?>
+
+</body>
+</html>
